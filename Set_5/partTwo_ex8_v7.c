aa38f5ac6d9489a4946eaa01265fb2f5fc2f07ea
monter <56112943+Mon-ter@users.noreply.github.com>
2021-04-24 16:21:47 +0200

    Create partTwo_ex8_v7.c

---
+++ Set_5/partTwo_ex8_v7.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char* argv[]) {
+	FILE* x;
+	char ware [100];
+	char name [16];
+	if (argc != 2) {
+		printf("default\n");
+		x = fopen("input.txt", "r");
+	} else {
+		x = fopen(argv[1], "r");
+	}
+	fscanf(x, "%s", name);
+	fclose(x);
+	int sum = 0;
+	int number = 0;
+	int min = 0;
+	int max = 0;
+	x = fopen(name, "r");
+	fscanf(x, "%s", ware);
+	if (!feof(x)) {
+		min = strlen(ware);
+		max = min;
+		sum = min;
+		number = 1;
+	}
+	fscanf(x, "%s", ware);
+	while (!feof(x)) {
+		int mL = strlen(ware);
+		if (mL > max) {
+			max = mL;
+		} else if (mL < min) {
+			min = mL;
+		}
+		sum += mL;
+		number++;
+		fscanf(x, "%s", ware);
+	}
+	fclose(x);
+	x = fopen("statistics.dat", "w");
+	fprintf(x, "avg: %.2f\nmin: %d\nmax: %d\n", sum * 1.0 / number, min, max);
+	fclose(x);
+	return 0;
+}
